693f6180ab1c6de41d09efe74d722bb781906cc1
Arnav Garg <arnavgarg495@gmail.com>
2025-11-30 12:20:03 +0530

    Add BFS function and main for graph traversal
    Implement BFS algorithm to traverse a graph.

---
+++ assigment9/1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+void BFS(int start, vector<vector<int>> &adj, int n) {
+    vector<bool> visited(n, false);
+    queue<int> q;
+
+    visited[start] = true;
+    q.push(start);
+
+    cout << "BFS: ";
+
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        cout << node << " ";
+
+        for (int x : adj[node]) {
+            if (!visited[x]) {
+                visited[x] = true;
+                q.push(x);
+            }
+        }
+    }
+}
+
+int main() {
+    int n = 5;
+    vector<vector<int>> adj(n);
+
+    adj[0] = {1, 2};
+    adj[1] = {0, 3};
+    adj[2] = {0, 4};
+    adj[3] = {1};
+    adj[4] = {2};
+
+    BFS(0, adj, n);
+}
